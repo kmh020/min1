dbfed5554c5c004efe59a86f17a174ef135e7db1
kmh020 <kmh020@gmail.com>
2013-10-14 00:14:04 +0900

    my first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('min1App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'minhyung'
+    ];
+  });
+
+
+angular.module('min1App')
+  .controller('MainCtrl3', function ($scope) {
+    $scope.awesomeThings = [
+      'Karma',
+      'Karma',
+      'Karma',
+      'Karma',
+      'minhyung'
+    ];
+  });
+
+
